4b21d23f5bef5395dfb879882538fd2bf2bc96ef
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-04-10 09:48:08 -0600

    feat: add basic feature handling in controllers and routes

--- src/main/java/altamirano/hernandez/app1_springboot_2025/controllers/ProductoController.java
+++ src/main/java/altamirano/hernandez/app1_springboot_2025/controllers/ProductoController.java
@@ -1,8 +1,9 @@
 package altamirano.hernandez.app1_springboot_2025.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,10 +12,70 @@ import java.util.Map;
 @RequestMapping("/productos")
 public class ProductoController {
 
+    //Variables globales de ambiente
+    @Value("${datos.nombre}")
+    String nombre;
+    @Value("${datos.apellidoPaterno}")
+    String apellidoPaterno;
+    @Value("${datos.apellidoMaterno}")
+    String apellidoMaterno;
+
+    //Inyectamos el objeto Enviroment
+    @Autowired
+    Environment env;
+
     @GetMapping("/prueba")
-    Map<String, Object> pruebas(){
+    Map<String, Object> pruebas() {
         Map<String, Object> json = new HashMap<>();
         json.put("test", "Funcionando el controlador de producto");
+        return json;
+    }
+
+    @GetMapping("/saludo")
+    public Map<String, Object> saludo() {
+        Map<String, Object> json = new HashMap<>();
+        json.put("saludo", "Saludo desde la ruta /saludo");
+        return json;
+    }
+
+    //Metodo con PathVariable obligatorio
+    @GetMapping("/parametros/{id}/{nombre}/{aP}/{aM}")
+    public Map<String, Object> pathVariable(@PathVariable int id, @PathVariable String nombre, @PathVariable String aP, @PathVariable String aM) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("Los datos que se enviaron en el pathVariable", "son los siguientes");
+        json.put("id", id);
+        json.put("nombre", nombre);
+        json.put("Apellido Paterno", aP);
+        json.put("Apellido Materno", aM);
+        return json;
+    }
+
+    //Metodo con RequestParam
+    @GetMapping("/requestParams")
+    public Map<String, Object> requestParams(@RequestParam int id, @RequestParam String ciudad) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("id", id);
+        json.put("ciudad", ciudad);
+        return json;
+    }
+
+    //Inyeccion de valores desde el archivo de ambiente
+    @GetMapping("/enviroment")
+    public Map<String, Object> enviroment() {
+        Map<String, Object> json = new HashMap<>();
+        json.put("Valores de variable de: ", "ambiente");
+        json.put("nombre", nombre);
+        json.put("apellidoPaterno", apellidoMaterno);
+        json.put("apellidoMaterno", apellidoMaterno);
+        return json;
+    }
+
+    //Inyeccion de variables a traves de objeto de tipo Enviroment
+    @GetMapping("/objetoEnviroment")
+    public Map<String, Object> objetoEnviroment() {
+        Map<String, Object> json = new HashMap<>();
+        String email = env.getProperty("datos.email");
+        json.put("email", email);
         return json;
     }
 }
